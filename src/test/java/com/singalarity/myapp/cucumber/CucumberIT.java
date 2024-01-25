587fcdbca2683365b8d13fad0a8d6de031e07bef
hodacdat <68152269+hodacdat@users.noreply.github.com>
2024-01-25 21:50:38 +0700

    1st init

---
+++ src/test/java/com/singalarity/myapp/cucumber/CucumberIT.java
@@ -0,0 +1,8 @@
+package com.singalarity.myapp.cucumber;
+
+import com.singalarity.myapp.IntegrationTest;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+@IntegrationTest
+class CucumberIT {}
